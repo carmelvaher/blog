46d78404a632937fc0dc1204c684caa5ed133d71
carmel.vaher <carmel.vaher@khk.ee>
2014-03-20 15:20:18 +0200

    Update controllers

--- controllers/posts.php
+++ controllers/posts.php
@@ -1,28 +1,38 @@
 <?php
 
-class posts extends Controller{
-	function index(){
-		$this->posts = get_all("SELECT * FROM post");
+class posts extends Controller
+{
+
+    function index()
+    {
+        $this->posts = get_all("SELECT * FROM post NATURAL JOIN user");
+
+//prepare tags array
         $_tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag");
-        foreach ($_tags as $tag){
+        foreach ($_tags as $tag) {
             $this->tags[$tag['post_id']][] = $tag['tag_name'];
         }
-	}
+    }
 
-    function index_ajax(){
-		echo "\$_POST:<br>";
+
+    function index_ajax()
+    {
+        echo "\$_POST:<br>";
         var_dump($_POST);
     }
 
-	function index_post(){
-		echo "\$_POST:<br>";
-		var_dump($_POST);
-	}
+    function index_post()
+    {
+        echo "\$_POST:<br>";
+        var_dump($_POST);
+    }
 
-    function view () {
+    function view()
+    {
         $post_id = $this->params[0];
         $this->post = get_first("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
         $this->tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
-    }
+        $this->comments = get_all("SELECT * FROM post_comments NATURAL JOIN comment WHERE post_id='$post_id'");
 
+    }
 }
