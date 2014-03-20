b85e9225e9b8df5e0a0da23c61dbf094513d459c
carmel.vaher <carmel.vaher@khk.ee>
2014-03-20 15:08:12 +0200

    Fix Tags

--- views/tags/tags_index.php
+++ views/tags/tags_index.php
@@ -1,6 +1,9 @@
-<?foreach( $tags as $tag ):?>
-    <div class="list-group">
-        <a href="#" class="list-group-item active"></a>
-        <a href="#" class="list-group-item"><?=BASE_URL?>tags/view/<?=$tag['tag_id']?>"><?=$tags['tag_name']?></a>
-    </div>
-<?endforeach?>
+
+<div class="list-group">
+    <?foreach ($tags as $tag):?>
+        <a
+            href="<?=BASE_URL?>tags/view/<?=$tag['tag_name']?>" class="list-group-item">
+            <?=$tag['tag_name']?>
+        </a>
+    <?endforeach?>
+</div>
