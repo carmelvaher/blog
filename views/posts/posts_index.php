f12701561f422ba8a2eb54a65d442c9384be379c
carmel.vaher <carmel.vaher@khk.ee>
2014-03-20 15:05:09 +0200

    Fix Posts

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -1,11 +1,28 @@
-<?foreach( $posts as $post ):?>
-<div class="span8">
-    <a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><h1><?=$post['post_subject']?></h1></a>
-    </div>
-    <div>
-    <p><?=$post['post_text']?></p>
-    </div>
-    <?endforeach?>
+<h1>Welcome!</h1>
+<!-- Code for ajax -->
+<script type="text/javascript">
+    function clickme() {
+        $.post("<?=BASE_URL?>welcome", $( "#ajax-form" ).serialize(), function (data) {
+            $(".result").html(data);
+        });
+    }
+</script>
 
-<?foreach ($tags[$post['post_id']]as $tag):?>
-    <a href="#"><span class="label" style="background-color: #5bc0de"><?=$tag?></span></a> <? endforeach?>
+
+
+<?foreach($posts as $post):?>
+    <div class="span8">
+        <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post["post_subject"]?></a></h1>
+        <p><?=$post["post_text"]?></p>
+        <div>
+            <span class="badge badge-success">Posted <?=$post["post_created"]?></span>
+            <span class="badge badge-success">By <?=$post["username"]?></span>
+            <div class="pull-right">
+                <?foreach ($tags   [$post	['post_id']] as $tag):?>
+                    <a href="<?=BASE_URL?>tags/view/<?=$tag?>"><span class = "label" style="background-color: #5bc0de"><?=$tag?></span></a>
+                <?endforeach?>
+            </div>
+        </div>
+        <hr>
+    </div>
+<?endforeach?>ag?></span></a> <? endforeach?>
