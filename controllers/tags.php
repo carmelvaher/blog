8482641aae6224780b498a2b68578f2144a726e1
carmel.vaher <carmel.vaher@khk.ee>
2014-04-03 15:07:47 +0300

    Correcting the code

--- controllers/tags.php
+++ controllers/tags.php
@@ -17,7 +17,7 @@ class tags extends Controller{
     function view(){
         $tag_name = $this->params[0];
         $tag_id = get_one("SELECT tag_id FROM tag NATURAL JOIN user WHERE tag_name='$tag_name'");
-        $this->posts = get_all("SELECT * FROM post_tags NATURAL JOIN post, user WHERE tag_id='$tag_id'");
+        $this->posts = get_all("SELECT * FROM post_tags NATURAL JOIN post NATURAL JOIN user WHERE tag_id='$tag_id'");
         $post_id = $this->params[0];
         $this->post = get_one("SELECT * FROM post NATURAL JOIN user WHERE post_id='$post_id'");
         $this->tags = get_all("SELECT * FROM post_tags NATURAL JOIN tag WHERE post_id='$post_id'");
