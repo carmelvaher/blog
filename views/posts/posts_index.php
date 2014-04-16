e6e5f681be21641c6c46c4c9e4e7ab1b61ccfa4a
Carmel Vaher <carmel.vaher@khk.ee>
2014-04-16 10:16:55 +0300

    Fixing error in posts_views tags

--- views/posts/posts_index.php
+++ views/posts/posts_index.php
@@ -15,10 +15,10 @@
         <h1><a href="<?=BASE_URL?>posts/view/<?=$post['post_id']?>"><?=$post["post_subject"]?></a></h1>
         <p><?=$post["post_text"]?></p>
         <div>
-            <span class="badge badge-success">Posted <?=$post["post_created"]?></span>
+                <span class="badge badge-success">Posted <?=$post["post_created"]?></span>
             <span class="badge badge-success">By <?=$post["username"]?></span>
-            <div class="pull-right">
-                <?foreach ($tags   [$post	['post_id']] as $tag):?>
+                <div class="pull-right">
+              <?foreach ($tags   [$post	['post_id']] as $tag):?>
                     <a href="<?=BASE_URL?>tags/view/<?=$tag?>"><span class = "label" style="background-color: #5bc0de"><?=$tag?></span></a>
                 <?endforeach?>
             </div>
